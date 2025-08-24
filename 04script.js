13cc98e0b754af36a54d8233e5ddb56f853d4c72
Nando1510 <117784292+Nando1510@users.noreply.github.com>
2025-08-24 17:50:28 -0300

    Create 04script.js

---
+++ 04script.js
@@ -0,0 +1,33 @@
+const botao_Converter = document.getElementById('converter');
+botao_Converter.addEventListener('click', conversao);
+
+const dolar = document.getElementById('dolar');
+dolar.value = 1000;
+
+
+async function conversao(event){
+    event.preventDefault();
+
+    const real = parseFloat(document.getElementById('real').value.trim());
+      
+
+    if (!real || !dolar.value){
+        alert('Preencha todos os campos!!');
+        return;
+    }
+
+    const cotacao = real / dolar.value
+
+    document.getElementById('resultado').innerText = `R$ ${cotacao.toFixed(2)}`
+   
+}
+
+const botao_Cancelar = document.getElementById('limpar')
+botao_Cancelar.addEventListener('click', cancelar);
+
+async function cancelar(event){
+    event.preventDefault();
+    document.getElementById('real').value='';
+    dolar.value = 1000;
+    document.getElementById('resultado').innerText = "$ 0.00"; 
+}
